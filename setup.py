4aa9a5a6414f92afbb60704d708f0982b69a330e
Ruslan Cheshko <chex@soul.li>
2013-12-29 19:22:49 +0200

    this will be django package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='django-soul',
+    version='0.1dev',
+    packages=['soul',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
