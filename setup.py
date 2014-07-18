af76ea93aa45ac00b044a5aff949e0e37fd3134e
Ruslan Cheshko <chex@soul.li>
2014-07-18 16:01:00 +0300

    up version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ INSTALL_REQUIRES = [
 
 setup(
     name='django-soul',
-    version='0.11dev',
+    version='0.12dev',
     packages=['soul',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
